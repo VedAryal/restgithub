fb22d0e97dfb70675f950c1a39c90771641d0863
Ved Aryal <aryalved1@gmail.com>
2019-11-01 09:27:29 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+  }
+  componentDidMount = () => {
+    fetch(`https://api.github.com/search/repositories?q=react`)
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({ data: responseData.items });
+      });
+  };
+  render() {
+    const tableRows = this.state.data.map(item => (
+      <tr key={item.full_name}>
+        <td>{item.full_name}</td>
+        <td>
+          <a href={item.html_url}>{item.html_url}</a>
+        </td>
+      </tr>
+    ));
+    return (
+      <div className="App">
+        <h1>Repositories</h1>
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>URL</th>
+            </tr>
+            {tableRows}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
